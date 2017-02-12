ef5e35da5cee15a070f374b0aa3d965c1559b987
kosalendra <kosalendra@outlook.com>
2017-02-12 14:14:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/page_one', function (req, res) {
+    res.send("Page one is bang on!");
+});
+
+app.get('/ui/page_one', function (req, res) {
+    res.send("Page one is bang on!");
+});
+
+app.get('/ui/page_two', function (req, res) {
+    res.send("Page two is bang on!");
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
