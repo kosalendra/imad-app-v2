5b604dbf1640ca470c9a7a1c5231f1bb0eccd6ae
kosalendra <kosalendra@outlook.com>
2017-03-09 00:39:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick = function() {
 		}
 	};
 	//Make the request
-	request.open('GET', 'http://localhost:8080/counter', true);
+	request.open('GET', 'http://http://kosalendra.imad.hasura-app.io/counter', true);
 	request.send(null);
 	
 };
